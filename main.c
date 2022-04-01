68db68f5f4af6b5dabe3a1f78b9afcd57e8a4abb
KIPASGTS <83524667+KIPASGTS@users.noreply.github.com>
2022-04-01 14:12:59 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include "BinaryReader_BinaryWriter.h"
 
 int main() {
+    //Writer Example
+    BinaryWriter writer;
+    writer.write_str("KIPASGTS");//name
+    writer.write_int(19);//age
+    writer.save_as("kipas.bin");
 
+    //Reader Example
+    BinaryReader reader;
+    string m_name = reader.read_str();
+    int m_age = reader.read_int();
+    cout << "Name: " << m_name << ", Age: " << m_age << endl;
 }
