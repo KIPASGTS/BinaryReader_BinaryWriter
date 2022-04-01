0f459ab700c2011a0948b75ffc5bf3bd7f8ccd58
KIPASGTS <83524667+KIPASGTS@users.noreply.github.com>
2022-04-01 14:13:53 +0700

    Update main.c

--- main.c
+++ main.c
@@ -12,4 +12,7 @@ int main() {
     string m_name = reader.read_str();
     int m_age = reader.read_int();
     cout << "Name: " << m_name << ", Age: " << m_age << endl;
+    
+    //Output
+    //Name: KIPASGTS, Age: 19
 }
